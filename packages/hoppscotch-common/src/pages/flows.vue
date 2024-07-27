c9841d59eb65184598901f4f91075960098d068a
Nagendran Nagarajan <nagendran@qburst.com>
2024-07-27 11:31:56 +0530

    feat: setup vue-flow for flows

---
+++ packages/hoppscotch-common/src/pages/flows.vue
@@ -0,0 +1,47 @@
+<template>
+  <VueFlow :nodes="nodes" :edges="edges"></VueFlow>
+</template>
+
+<script setup>
+import { ref } from "vue"
+import { VueFlow } from "@vue-flow/core"
+
+const nodes = ref([
+  {
+    id: "1",
+    type: "input",
+    position: { x: 250, y: 5 },
+    data: { label: "Node 1" },
+  },
+  {
+    id: "2",
+    position: { x: 100, y: 100 },
+    data: { label: "Node 2" },
+  },
+  {
+    id: "3",
+    type: "output",
+    position: { x: 400, y: 200 },
+    data: { label: "Node 3" },
+  },
+])
+
+const edges = ref([
+  {
+    id: "e1->2",
+    source: "1",
+    target: "2",
+  },
+  {
+    id: "e2->3",
+    source: "2",
+    target: "3",
+    animated: true,
+  },
+])
+</script>
+
+<style>
+@import "@vue-flow/core/dist/style.css";
+@import "@vue-flow/core/dist/theme-default.css";
+</style>
